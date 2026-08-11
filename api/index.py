663d822fdf67f52dc91be0c71676aacf8d20fb6c
Vishal Karthikeyan P <vishalkarthikeyanp@gmail.com>
2026-08-11 10:58:32 +0530

    Configure Flask app with custom template directory
    Updated Flask app to use a custom template folder for rendering.

--- api/index.py
+++ api/index.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 import requests
+import os
 
-app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR)
 
 BOOKS_URL = "https://drive.google.com/uc?export=download&id=1GwKEkGCxFPwV1HIl37bKZMofoPypNZfY"
 
@@ -11,9 +15,11 @@ def home():
     try:
         response = requests.get(BOOKS_URL, timeout=10)
         response.raise_for_status()
+
         books = response.json()
+
     except Exception as e:
-        print("Error loading books:", e)
+        print("ERROR LOADING BOOKS:", str(e))
         books = []
 
     return render_template("index.html", books=books)
