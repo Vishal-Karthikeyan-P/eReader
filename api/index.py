e7ea9875297855f53622a0802a1b1f1dbf9fec0c
Vishal Karthikeyan P <vishalkarthikeyanp@gmail.com>
2026-08-18 11:52:30 +0530

    Refactor api/index.py for improved structure

--- api/index.py
+++ api/index.py
@@ -279,7 +279,7 @@ def setup():
 # Bookshelf Page
 @app.route("/bookshelf")
 def bookshelf():
-    return render_template("bookshelf")
+    return render_template("bookshelf.html")
 
 
 # Reader Page
